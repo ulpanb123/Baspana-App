6efd999993d465e25b4e698aabf6df69cb3a8965
Ulpan <ulpan.beken.02@gmail.com>
2021-10-09 12:17:27 +0600

    n-commit

--- app/src/main/java/com/example/baspana1/main/home/adapter/AdvertsAdapter.kt
+++ app/src/main/java/com/example/baspana1/main/home/adapter/AdvertsAdapter.kt
@@ -1,19 +1,19 @@
 package com.example.baspana1.main.home.adapter
 
+import android.os.Build
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.net.toUri
-import androidx.databinding.DataBindingUtil
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.baspana1.R
 import com.example.baspana1.databinding.ItemAdvertsBinding
 import com.example.baspana1.model.adverts.AdvertItem
-import com.example.baspana1.model.adverts.Adverts
+import java.text.NumberFormat
+import java.util.*
 
 class AdvertsAdapter : RecyclerView.Adapter<AdvertsAdapter.AdvertsViewholder>() {
 
@@ -23,18 +23,19 @@ class AdvertsAdapter : RecyclerView.Adapter<AdvertsAdapter.AdvertsViewholder>() 
 
     }
 
-    fun setAdverts(adverts : List<AdvertItem>) {
+    fun setAdverts(adverts: List<AdvertItem>) {
         this.adverts = adverts.toMutableList()
         notifyDataSetChanged()
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onBindViewHolder(holder: AdvertsAdapter.AdvertsViewholder, position: Int) {
         val advert = adverts[position]
         bindImage(holder.binding.buildingImageView, advert.images[0].image)
-        holder.binding.createdDateTextView.text = advert.created_at
+        holder.binding.createdDateTextView.text = advert.created_at.subSequence(0, 10)
         holder.binding.imagesCountTextView.text = "1/${advert.images.size}"
-        holder.binding.titleTextView.text = advert.price
+        bindPrice(holder.binding.titleTextView, advert.price)
         holder.binding.shortInfoTextView.text = "${advert.room_count}-комн. дом " +
                 "· ${advert.total_area} м² " +
                 "· ${advert.flat_floor}/${advert.house_floor} этаж"
@@ -62,6 +63,15 @@ class AdvertsAdapter : RecyclerView.Adapter<AdvertsAdapter.AdvertsViewholder>() 
                             .error(R.drawable.ic_broken_image))
                     .into(imgView)
 
+    }
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun bindPrice(textView: TextView, price: String) {
+        val format: NumberFormat = NumberFormat.getCurrencyInstance()
+        format.setMaximumFractionDigits(0)
+        format.setCurrency(Currency.getInstance("KZT"))
+
+        val newPrice = format.format(price.toInt())
+        textView.text = newPrice
     }
 
 }
